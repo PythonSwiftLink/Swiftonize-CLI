ea875660661ebb76c2813fbaea38e6e7bab7453f
psychowaspx <psychowaspx@gmail.com>
2023-03-28 13:43:11 +0200

    python standard library added

--- SwiftonizeCLI/SwiftonizeCLI/cli.swift
+++ SwiftonizeCLI/SwiftonizeCLI/cli.swift
@@ -5,11 +5,30 @@ import ArgumentParser
 import Swiftonize
 
 
+func DEBUG_PRINT(_ items: Any..., separator: String = " ", terminator: String = "\n") {
+#if DEBUG
+    print(items, separator: separator, terminator: terminator)
+#endif
+}
+
+
+//let APP_FOLDER = FileManager.default.urls(for: .applicationDirectory, in: .localDomainMask).first!.appendingPathComponent("PythonSwiftLinkCLI")
+
+let APP_FOLDER = (Bundle.main.executableURL!).deletingLastPathComponent()
+
+
+var PYTHON_EXTRA_MODULES : [String] {
+    [
+        APP_FOLDER.appendingPathComponent("python-extra").path,
+        //(SWIFT_TOOLS + "src" ).string
+    ]
+}
+
 @main
 struct SwiftonizeCLI: AsyncParsableCommand {
     init() {
         //        try await checkSwiftTools()
-        //        PythonHandler.shared.defaultRunning.toggle()
+                PythonHandler.shared.defaultRunning.toggle()
     }
     
     
@@ -28,14 +47,15 @@ struct SwiftonizeCLI: AsyncParsableCommand {
         @Argument() var destination: String
         
         func run() async throws {
+            print(APP_FOLDER)
             let src = URL(fileURLWithPath: source)
-            
+
             let filename = src.lastPathComponent.replacingOccurrences(of: ".py", with: "")
-            
+
             let dst = URL(fileURLWithPath: destination).appendingPathComponent("\(filename).swift")
-            
+
             let code = try String(contentsOf: src)
-            
+
             let module = await WrapModule(fromAst: filename, string: code, swiftui: false)
             try module.pySwiftCode.write(to: dst, atomically: true, encoding: .utf8)
         }
